76b69ee83512b6f6c35c2cae75165cc753f299ce
raja <rajarajagarib123@gmail.com>
2025-11-17 21:02:46 +0100

    modifier l'url

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://backend-8tpe.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL || "https://backend-8tpe.onrender.com/api",
   headers: {
 
     "Content-Type": "application/json",
